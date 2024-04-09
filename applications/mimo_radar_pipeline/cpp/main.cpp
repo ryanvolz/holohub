be0dc715566c56cc0f73646b8d8cec5f3d313e89
Ryan Volz <rvolz@mit.edu>
2024-04-09 13:47:42 -0400

    Enable 4 EMVSIS streams of 100 kHz data at separate frequencies.

--- applications/mimo_radar_pipeline/cpp/main.cpp
+++ applications/mimo_radar_pipeline/cpp/main.cpp
@@ -32,6 +32,7 @@ class App : public holoscan::Application {
     HOLOSCAN_LOG_INFO("Initializing radar pipeline as data processor");
 
     std::shared_ptr<holoscan::Operator> last_op;
+    std::shared_ptr<holoscan::Operator> branch_op;
 
     auto adv_net_rx =
         make_operator<ops::AdvNetworkOpRx>("adv_network_rx",
@@ -54,24 +55,83 @@ class App : public holoscan::Application {
       auto converter0 = make_operator<ops::ComplexIntToFloatOp>("converter0");
       add_flow(last_op, converter0);
       last_op = converter0;
+      branch_op = last_op;
 
-      if (from_config("pipeline.rotator0").as<bool>()) {
-        auto rotator0 =
-            make_operator<ops::ScheduledRotatorOp>("rotator0", from_config("ScheduledRotatorOp0"));
-        add_flow(last_op, rotator0);
-        last_op = rotator0;
+      // A
+      if (from_config("pipeline.rotator0a").as<bool>()) {
+        auto rotator0a = make_operator<ops::ScheduledRotatorOp>(
+            "rotator0a", from_config("ScheduledRotatorOp0a"));
+        add_flow(branch_op, rotator0a);
+        last_op = rotator0a;
       }
 
-      if (from_config("pipeline.resample0").as<bool>()) {
-        auto resample0 =
-            make_operator<ops::ResamplePolyOp>("resample0", from_config("ResamplePolyOp0"));
-        add_flow(last_op, resample0);
-        last_op = resample0;
+      if (from_config("pipeline.resample0a").as<bool>()) {
+        auto resample0a =
+            make_operator<ops::ResamplePolyOp>("resample0a", from_config("ResamplePolyOp0"));
+        add_flow(last_op, resample0a);
+        last_op = resample0a;
       }
 
-      auto drf_sink0 = make_operator<ops::DigitalRFSinkOp<complex_t>>(
-          "drf_sink0", from_config("DigitalRFSinkOp0"));
-      add_flow(last_op, drf_sink0);
+      auto drf_sink0a = make_operator<ops::DigitalRFSinkOp<complex_t>>(
+          "drf_sink0a", from_config("DigitalRFSinkOp0a"));
+      add_flow(last_op, drf_sink0a);
+
+      // B
+      if (from_config("pipeline.rotator0b").as<bool>()) {
+        auto rotator0b = make_operator<ops::ScheduledRotatorOp>(
+            "rotator0b", from_config("ScheduledRotatorOp0b"));
+        add_flow(branch_op, rotator0b);
+        last_op = rotator0b;
+      }
+
+      if (from_config("pipeline.resample0b").as<bool>()) {
+        auto resample0b =
+            make_operator<ops::ResamplePolyOp>("resample0b", from_config("ResamplePolyOp0"));
+        add_flow(last_op, resample0b);
+        last_op = resample0b;
+      }
+
+      auto drf_sink0b = make_operator<ops::DigitalRFSinkOp<complex_t>>(
+          "drf_sink0b", from_config("DigitalRFSinkOp0b"));
+      add_flow(last_op, drf_sink0b);
+
+      // C
+      if (from_config("pipeline.rotator0c").as<bool>()) {
+        auto rotator0c = make_operator<ops::ScheduledRotatorOp>(
+            "rotator0c", from_config("ScheduledRotatorOp0c"));
+        add_flow(branch_op, rotator0c);
+        last_op = rotator0c;
+      }
+
+      if (from_config("pipeline.resample0c").as<bool>()) {
+        auto resample0c =
+            make_operator<ops::ResamplePolyOp>("resample0c", from_config("ResamplePolyOp0"));
+        add_flow(last_op, resample0c);
+        last_op = resample0c;
+      }
+
+      auto drf_sink0c = make_operator<ops::DigitalRFSinkOp<complex_t>>(
+          "drf_sink0c", from_config("DigitalRFSinkOp0c"));
+      add_flow(last_op, drf_sink0c);
+
+      // D
+      if (from_config("pipeline.rotator0d").as<bool>()) {
+        auto rotator0d = make_operator<ops::ScheduledRotatorOp>(
+            "rotator0d", from_config("ScheduledRotatorOp0d"));
+        add_flow(branch_op, rotator0d);
+        last_op = rotator0d;
+      }
+
+      if (from_config("pipeline.resample0d").as<bool>()) {
+        auto resample0d =
+            make_operator<ops::ResamplePolyOp>("resample0d", from_config("ResamplePolyOp0"));
+        add_flow(last_op, resample0d);
+        last_op = resample0d;
+      }
+
+      auto drf_sink0d = make_operator<ops::DigitalRFSinkOp<complex_t>>(
+          "drf_sink0d", from_config("DigitalRFSinkOp0d"));
+      add_flow(last_op, drf_sink0d);
     } else {
       auto drf_sink0 = make_operator<ops::DigitalRFSinkOp<sample_t>>(
           "drf_sink0", from_config("DigitalRFSinkOp0"));

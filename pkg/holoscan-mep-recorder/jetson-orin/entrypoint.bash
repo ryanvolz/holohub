7032b6ceddeb57ac335a2b477afc9404033488ed
Ryan Volz <rvolz@mit.edu>
2025-05-08 13:25:44 -0400

    Installation and docker fixes

--- pkg/holoscan-mep-recorder/jetson-orin/entrypoint.bash
+++ pkg/holoscan-mep-recorder/jetson-orin/entrypoint.bash
@@ -7,4 +7,4 @@ trap "trap - SIGTERM && kill -SIGTERM -$$" SIGINT SIGTERM EXIT
 # mirror recordings in working directory (expected to be ramdisk) to managed ringbuffer storage
 drf mirror mv . /data/ringbuffer/ &
 
-python /opt/holohub/applications/sdr_mep_recorder/sdr_mep_recorder.py "$@"
+python3 /opt/nvidia/holoscan/bin/sdr_mep_recorder.py "$@"

41671825dc5b44a7b6bde4bbe5929a7fa5cd6ce8
Ryan Volz <rvolz@mit.edu>
2025-07-25 17:16:19 -0400

    I guess the Jetson can't handle parallel builds anymore

--- build_sdr_mep_recorder.sh
+++ build_sdr_mep_recorder.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./run build sdr_mep_recorder --buildpath build --configure-args "-DCMAKE_VERBOSE_MAKEFILE=OFF" 2>&1 --type release --parallel 2 | tee bld.log
+./run build sdr_mep_recorder --buildpath build --configure-args "-DCMAKE_VERBOSE_MAKEFILE=OFF" 2>&1 --type release --parallel 1 | tee bld.log
